f021885a9518d856081323a85982036a4ce71fb8
DigitalDjinni <michael.isenhour@snhu.edu>
2025-02-21 19:18:05 -0500

    finalized comments and jinja
    Added comments and created jinja pages for news, meals, contact, and other non-essential pages

--- Travlr/app/models.py
+++ Travlr/app/models.py
@@ -1,14 +1,14 @@
 from app import db  
 from flask_login import UserMixin
 
-
+# User model for storing user data in the database.
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
     is_admin = db.Column(db.Boolean, default=False) # Setting the default to user. Role is changed inside the database
 
-
+# Trip model for storing trip data in the database
 class Trip(db.Model):
     __tablename__ = 'trips'
     id = db.Column(db.Integer, primary_key=True)
